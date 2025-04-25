392267db80f71904f8a5acf214b8fd9dd465bcb6
Maïwenn Plevenage <plevenagemaiwenn@gmail.com>
2025-04-25 11:49:36 +0200

    Script pour retirer les lignes sans 'on'

---
+++ constitcorpus.py
@@ -0,0 +1,27 @@
+import regex as re
+
+def filtre_corpus():
+    with open('B2data_essai.csv' ,'r') as f :
+        corpus = f.readlines()
+        corpus_filtre = []
+        for line in corpus :
+            balises = re.compile(r"<(i|n|a)>.*<\/(i|n|a)>")
+            result = balises.search(line)
+            if result != None :
+                corpus_filtre.append(line)  
+        return corpus_filtre
+
+
+def write_corpus(corpus):
+    with open('corpus_filtre.txt', 'w') as f :
+        for line in corpus : 
+            f.write(line+",")
+
+def main():
+
+    corpus = filtre_corpus()
+    write_corpus(corpus)
+
+if __name__ == "__main__":
+    main()
+             
